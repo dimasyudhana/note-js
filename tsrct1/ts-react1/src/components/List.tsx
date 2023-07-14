d335200e2b9d41d73972dd20fe09f937eab112f6
dimasyudhana <dimas.yudhana@gmail.com>
2023-07-15 00:14:00 +0800

    intro-react-component

---
+++ tsrct1/ts-react1/src/components/List.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from 'react'
+
+interface ListProps<T> {
+    items: T[],
+    render: (item: T) => ReactNode
+}
+
+
+// put an empty object atau <T,> pakai koma
+const List = <T extends {}>({ items, render }: ListProps<T>) => {
+    return (
+        <ul>
+            {items.map((item, i) => (
+                <li key={i}>
+                    {render(item)}
+                </li>
+            ))}
+        </ul>
+    )
+}
+export default List
